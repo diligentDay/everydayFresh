3815ecbe2c4a2d06bd012087df9dd2313f581678
xwp <xwp_fullstack@163.com>
2017-07-09 20:59:47 +0800

    register模板视图

---
+++ everydayFresh/user/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls import include, url
+from . import views
+
+urlpatterns = [
+    url(r'^register/$', views.register),
+    url(r'^login/$', views.login),
+    url(r'register_handle/', views.register_handle)
+]
--- everydayFresh/user/views.py
+++ everydayFresh/user/views.py
@@ -1,3 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import *
+from hashlib import sha1
 
 # Create your views here.
+
+def register(request):
+    context = {'title': '注册', }
+
+    return render(request, 'user/register.html', context)
+
+
+def login(request):
+    context = {'title': '登陆', }
+
+    return render(request, 'user/login.html', context)
+
+
+def register_handle(request):
+    # 接收数据
+    post = request.POST
+    uname = post.get('user_name')
+    upwd = post.get('user_pwd')
+    umail = post.get('user_email')
+    # 加密
+    s1 = sha1()
+    s1.update(upwd)
+    upwd_sha1 = s1.hexdigest()
+    # 创建对象
+    user = UserInfo()
+    user.uname = uname
+    user.upwd = upwd_sha1
+    user.umail = umail
+    user.save()
+    # 完成转向
+    return redirect('/user/login/')
+
